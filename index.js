5b60413c1bab51076b704acd31cc23d87f831c43
sikim <sikim@m114.co.kr>
2021-02-17 12:45:52 +0900

    loadscript

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 'use strict'
-console.log('hahaho')
+;(function(window) {
+  // from https://github.com/tserkov/vue-plugin-load-script/blob/master/index.js
+  let loadScript = function (src) {
+    return new Promise(function (resolve, reject) {
+      let shouldAppend = false
+      let el = document.querySelector('script[src="' + src + '"]')
+      if (!el) {
+        el = document.createElement('script')
+        el.type = 'text/javascript'
+        el.async = true
+        el.src = src
+        shouldAppend = true
+      } else if (el.hasAttribute('data-loaded')) {
+        resolve(el)
+        return
+      }
+
+      el.addEventListener('error', reject)
+      el.addEventListener('abort', reject)
+      el.addEventListener('load', function loadScriptHandler() {
+        el.setAttribute('data-loaded', true)
+        resolve(el)
+      })
+
+      if (shouldAppend) { document.head.appendChild(el) }
+    })
+  }
+  // eslint-disable-next-line no-unused-vars
+  let unloadScript = function (src) {
+    // eslint-disable-line no-param-reassign
+    return new Promise(function (resolve, reject) {
+      const el = document.querySelector('script[src="' + src + '"]')
+
+      if (!el) {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject()
+        return
+      }
+
+      document.head.removeChild(el)
+
+      resolve()
+    })
+  }
+  loadScript('https://unpkg.com/requirejs@2.3.6/require.js')
+    .then(() => {
+      console.log(window.require)
+    })
+})(window)
